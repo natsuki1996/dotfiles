430d84fd14c1a2ae4e943bab00c34405287e1134
natsuki1996 <natsu1580ki@gmail.com>
2020-04-23 01:07:24 +0900

    fix install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -13,18 +13,19 @@ link_to_homedir() {
     command mkdir "$HOME/.dotbackup"
   fi
 
-  local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-  local dotdir=$(dirname ${script_dir})
+  local script_dir, dotdir
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+  dotdir=$(dirname "$script_dir")
   if [[ "$HOME" != "$dotdir" ]];then
-    for f in $dotdir/.??*; do
-      [[ `basename $f` == ".git" ]] && continue
-      if [[ -L "$HOME/`basename $f`" ]];then
-        command rm -f "$HOME/`basename $f`"
+    for f in "$dotdir"/.??*; do
+      [[ $(basename "$f") == ".git" ]] && continue
+      if [[ -L "$HOME/$(basename "$f")" ]];then
+        command rm -f "$HOME/$(basename "$f")"
       fi
-      if [[ -e "$HOME/`basename $f`" ]];then
-        command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
+      if [[ -e "$HOME/$(basename "$f")" ]];then
+        command mv "$HOME/$(basename "$f")" "$HOME/.dotbackup"
       fi
-      command ln -snf $f $HOME
+      command ln -snf "$f" "$HOME"
     done
   else
     command echo "same install src dest"
@@ -47,5 +48,5 @@ while [ $# -gt 0 ];do
 done
 
 link_to_homedir
-git config --global include.path "~/.gitconfig_shared"
+git config --global include.path "$HOME/.gitconfig_shared"
 command echo -e "\e[1;36m Install completed!!!! \e[m"
