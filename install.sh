117402f6b3c8b548d858972a1db55079fa8a7585
natsuki1996 <natsu1580ki@gmail.com>
2020-04-24 09:58:51 +0900

    fix install

--- install.sh
+++ install.sh
@@ -2,12 +2,20 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+link_safe () {
+  if [ ! -L "$2" ]; then
+    ln -s "$1" "$2"
+  else
+    echo "$2" exist.
+  fi
+}
+
 # vim
 # ln -s "${BASEDIR}"/vimrc ~/.vimrc
 # ln -s "${BASEDIR}"/vim/ ~/.vim
 
 # zsh
-ln -s "${BASEDIR}"/zshrc ~/.zshrc
+link_safe "${BASEDIR}"/.zshrc ~/.zshrc
 
 # git
 # ln -s "${BASEDIR}"/gitconfig ~/.gitconfig
